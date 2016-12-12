b25d11c4837aae00a21cf4503b2b77c84b4f53dc
jdomenechb <jdomenechbborges@gmail.com>
2016-12-12 01:08:58 +0100

    Updated vagrant route script

--- vagrant/route.sh
+++ vagrant/route.sh
@@ -2,10 +2,18 @@
 
 DEFAULTROUTE=$(ip route show | grep 10.0.2.2 | wc -l)
 
+isSemantic=$(ifconfig -a | grep eth0)
+
+if [ "$isSemantic" != "" ]; then
+    interf="eth0"
+else
+    interf="enp0s3"
+fi
+
 if [ $DEFAULTROUTE -eq 0 ]; then
-    sudo ip route add default via 10.0.2.2 dev eth0
+    sudo ip route add default via 10.0.2.2 dev $interf
 fi
 
 if [ $DEFAULTROUTE -gt 0 ]; then
-    sudo ip route chg default via 10.0.2.2 dev eth0
+    sudo ip route chg default via 10.0.2.2 dev $interf
 fi
