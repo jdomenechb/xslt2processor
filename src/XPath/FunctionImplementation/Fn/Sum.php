1d0814dcdfe3ca9c544948266b03b416d32fa09f
jdomenechb <jdomenechbborges@gmail.com>
2017-05-07 16:40:25 +0200

    fn:sum() first implementation

---
+++ src/XPath/FunctionImplementation/Fn/Sum.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * This file is part of the XSLT2Processor package.
+ *
+ * (c) Jordi Domènech Bonilla
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Jdomenechb\XSLT2Processor\XPath\FunctionImplementation\Fn;
+
+use Jdomenechb\XSLT2Processor\XPath\FunctionImplementation\AbstractFunctionImplementation;
+use Jdomenechb\XSLT2Processor\XPath\XPathFunction;
+
+/**
+ * Function fn:sum() from XSLT standard library.
+ */
+class Sum extends AbstractFunctionImplementation
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @param XPathFunction $func
+     * @param $context
+     *
+     * @return string
+     */
+    public function evaluate(XPathFunction $func, $context)
+    {
+        $sumValues = $func->getParameters()[0]->evaluate($context);
+
+        $sumResult = 0;
+
+        foreach ($sumValues as $sumValue) {
+            $sumResult += $sumValue->nodeValue;
+        }
+
+        return $sumResult;
+    }
+}
